85c3edda95a46812e4321cd0137d7f28341d5ae2
gregbabbert <60162794+gregbabbert@users.noreply.github.com>
2020-11-09 23:01:18 -0500

    Make cyberdemon class

---
+++ CyberDemon.java
@@ -0,0 +1,35 @@
+
+
+/**
+ * The CyberDemon class is a subclass of the Creature class. It will never actually get instantiated,
+ * it's only purpose if to create a new Damage method for the CyberDemon subclasses to use
+ * 
+ * @author Greg Babbert
+ * @version 11.9.2020
+ */
+public class CyberDemon extends Demon
+{
+    // instance variables - replace the example below with your own
+    private static final int MAX_CYBER_HP = 100;
+    private static final int MIN_CYBER_HP = 25;
+    private static final int MAX_CYBER_STR = 40;
+    private static final int MIN_CYBER_STR = 20;
+    
+    /**
+     * Constructor for objects of class CyberDemon -
+     * Note that the calling class does not need to know anything about the 
+     * requirements of CyberDemon minimum and maximum values
+     * 
+     * The instantiating class asks for a CyberDemon and the CyberDemon class is responsible for
+     * return a CyberDemon object with values in the appropriate range
+     * 
+     */
+    public CyberDemon() {
+       super(
+            Randomizer.nextInt(MAX_CYBER_HP-MIN_CYBER_HP)+MIN_CYBER_HP,    
+            Randomizer.nextInt(MAX_CYBER_STR-MIN_CYBER_STR)+MIN_CYBER_STR
+        );   
+    }
+
+    
+}
--- Demon.java
+++ Demon.java
@@ -9,7 +9,36 @@
  */
 public class Demon extends Creature
 {
+    // instance variables - replace the example below with your own
+    private static final int MAX_HUMAN_HP = 30;
+    private static final int MIN_HUMAN_HP = 10;
+    private static final int MAX_HUMAN_STR = 18;
+    private static final int MIN_HUMAN_STR = 5;
+    private int str;
+    private int hp;
     
+    /**
+     * Constructor for objects of class Human -
+     * Note that the calling class does not need to know anything about the 
+     * requirements of human minimum and maximum values
+     * 
+     * The instantiating class asks for a Human and the human class is responsible for
+     * return a Human object with values in the appropriate range
+     * 
+     */
+    public Demon(int str, int hp) {
+       super(
+            str, hp
+        );
+    
+        // note how the class uses the static randomizer class to
+        // generate the values. This localizes the need to know 
+        // max and min values to this class only
+        // max-min is range of values
+        // range + min ensures that the values don't start at one.
+        
+          
+    }
     /**
      * Creates the number used for the attack method
      * @return a random value between a Demon sublcasses' minimum strength and its maximum strength,
